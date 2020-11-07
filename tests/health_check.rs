a6a5456e32d60972032f1a138d19542b29f194e2
Gary Holland <garyhollandxyz@gmail.com>
2020-11-07 11:43:38 +0000

    chore: break lib into mods

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,6 @@
 use reqwest;
 use std::net::TcpListener;
+use zero2prod::startup::run;
 
 /// Spin up an instance of our application
 /// and returns its address (i.e. http://localhost:XXXX)
@@ -7,7 +8,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
