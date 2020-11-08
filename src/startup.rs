4704c63bdec832e0102edf30264acfcb536d2e39
Gary Holland <garyhollandxyz@gmail.com>
2020-11-08 13:41:39 +0000

    telemetry setup

--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,13 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .data(db_pool.clone())
